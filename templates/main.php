a56741323607f484ee28787aea0f5c3c5d1b6cc4
Isak Combrinck <isak.combrinck@gmail.com>
2021-11-09 19:44:58 +0100

    Remove typography from template

--- templates/main.php
+++ templates/main.php
@@ -19,7 +19,6 @@ if ($settings['php_scripts'] != null) {
 		<link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,400&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital@0;1&display=swap" rel="stylesheet">
 
     <?php echo $settings['link_stylesheets']?>
 
@@ -32,16 +31,6 @@ if ($settings['php_scripts'] != null) {
     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#212121">
     <meta name="msapplication-TileColor" content="#2d89ef">
     <meta name="theme-color" content="#ffffff">
-
-        <style>
-            body, textarea, option, input, select, button {
-                font-family: 'Lato', sans-serif;
-            }
-
-            header.hero {
-                font-family: 'Cormorant Garamond';
-            }
-        </style>
 	</head>
 
 	<body>
